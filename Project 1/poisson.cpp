d51b16921b04e1592385e31d8a8be7608ffd3853
kingsman142 <jamhahn@yahoo.com>
2018-09-19 15:45:24 -0400

    pretty much almost finished with project 1

--- Project 1/poisson.cpp
+++ Project 1/poisson.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
+#include <iomanip>
 using namespace std;
 
-double[] subtract_vectors(double[], double[]);
-double calc_norm(double[], double[], int);
-double[] solve_jacobi();
-double[] initialize_random_vector(int);
+#define vvd vector< vector<double> >
+
+vector<double> subtract_vectors(vector<double>, vector<double>);
+double calc_norm(vector<double>, vector<double>, int);
+void solve_jacobi();
+vvd solve_gauss_seidel(int, vvd, int, int, double, double);
+vvd initialize_arr(int, int);
+vvd set_boundary_values(vvd, int, int, double, double, vvd l(vvd, int, double), vvd t(vvd, int, double), vvd r(vvd, int, int, double), vvd b(vvd, int, int, double));
+vvd verif_left(vvd, int, double);
+vvd verif_top(vvd, int, double);
+vvd verif_right(vvd, int, int, double);
+vvd verif_bottom(vvd, int, int, double);
+void print_solution(vvd);
 
 int main(int argc, char* argv[]){
 	int x, y;
@@ -15,75 +26,85 @@ int main(int argc, char* argv[]){
 	cout<<"Enter y dimension: ";
 	cin >> y;
 
-	int left = 0;
-	int top = 1;
-	int right = 1;
-	int bottom = 0;
+	double xConstraint = 2.0;
+	double yConstraint = 1.0;
+	double dx = (double) xConstraint / x;
+	double dy = (double) yConstraint / y;
 
-	double poissonMatrix[y][x];
-	double initVariables[x] = initialize_random_vector(x, 0, 100);
-	double gaussSeidel[y] = solve_gauss_seidel(1000, x, initVariables, poissonMatrix, ..., 1.0);
+	vvd poissonMatrix = initialize_arr(y, x);
+	poissonMatrix = set_boundary_values(poissonMatrix, y, x, dx, dy, verif_left, verif_top, verif_right, verif_bottom);
+	print_solution(poissonMatrix);
+	cout<<endl;
+	poissonMatrix = solve_gauss_seidel(1000, poissonMatrix, y, x, dx, dy);
+	print_solution(poissonMatrix);
 
 	return 0;
 }
 
-double
+void print_solution(vvd a){
+	for(int i = 0; i < a.size(); i++){
+		for(int j = 0; j < a[0].size(); j++){
+			cout<<a[i][j]<<" ";
+		}
+		cout<<endl;
+	}
+}
 
 // TODO: Implement
-double[] solve_jacobi(){
-	return 0;
+void solve_jacobi(){
+	return;
 }
 
-double[] solve_gauss_seidel(int iters, int numVars, double XO[], double a[][], double b[], double tolerance){
-	int k = 1;
-	double x[numVars];
-
-	while(k <= iters){
-		for(int i = 0; i < numVars; i++){
-			double firstSum = 0.0;
-			double secondSum = 0.0;
-			for(int j = 0; j < i; j++){
-				firstSum += a[i][j]*x[j];
+vvd solve_gauss_seidel(int iters, vvd a, int m, int n, double dx, double dy){
+	for(int iter = 0; iter < iters; iter++){
+		vvd output(a);
+		for(int i = 1; i < m-1; i++){
+			for(int j = 1; j < n-1; j++){
+				double below = a[i+1][j];
+				double here = a[i][j];
+				double above = a[i-1][j];
+				double right = a[i][j+1];
+				double left = a[i][j-1];
+				if(iter == 0) cout<<below<<" "<<here<<" "<<above<<" "<<right<<" "<<left<<endl;
+				//output[i][j] = (below - 2*here - above) / (dy*dy) + (right - 2*here + left) / (dx*dx);
+				output[i][j] = -(1.0/4.0)*(-(i*dx)*pow(dx, 2) + a[i+1][j] + a[i-1][j] + a[i][j-1] + a[i][j+1]);
+				if(iter == 0) cout<<output[i][j]<<endl;
 			}
-			for(int j = i; j < iters; j++){
-				secondSum += a[i][j]*XO[j];
-			}
-			x[i] = (1.0/a[i][i]) * (-firstSum - secondSum + b[i]); // Finish this line
 		}
-
-		double vectorDiff[] = subtract_vectors(x, XO);
-		double vecNorm = calc_norm(vectorDiff, 2); // euclidean norm
-		if(vecNorm < tolerance){
-			return x; // We solved it
-		}
-		k++;
-		for(int i = 0; i < numVars; i++){
-			XO[i] = x[i];
-		}
+		a = output;
 	}
 
-	return x;
+	return a;
 }
 
-double[] initialize_random_vector(int n, int min, int max){
-	double output[n];
-	for(int i = 0; i < n; i++){
-		double randVal = ((rand() % 100) / 100.0) * (max - min) + min;
-		output[i] = randVal;
+vvd set_boundary_values(vvd a, int m, int n, double dx, double dy, vvd left(vvd, int, double), vvd top(vvd, int, double), vvd right(vvd, int, int, double), vvd bottom(vvd, int, int, double)){
+	a = left(a, m, dy);
+	a = top(a, n, dx);
+	a = right(a, m, n, dy);
+	a = bottom(a, m, n, dx);
+	return a;
+}
+
+vvd initialize_arr(int m, int n){
+	vvd output(m, vector<double>(n, 0));
+	for(int i = 0; i < m; i++){
+		for(int j = 0; j < n; j++){
+			output[i][j] = 0;
+		}
 	}
 
 	return output;
 }
 
 // Subtract two vectors from each other
-double[] subtract_vectors(double[] t1, double[] t2){
+vector<double> subtract_vectors(vector<double> t1, vector<double> t2){
 	int t1Size = t1.size();
 	int t2Size = t2.size();
 	if(t1Size != t2Size){
-		return NULL;
+		return t1;
 	}
 
-	double output[t1Size];
+	vector<double> output(t1Size);
 	for(int i = 0; i < t1Size; i++){
 		int diff = t2[i] - t1[i];
 		output[i] = diff;
@@ -93,11 +114,43 @@ double[] subtract_vectors(double[] t1, double[] t2){
 }
 
 // Calculate the p-norm between two vectors t1 and t2
-double calc_norm(double vec[], double p){
+double calc_norm(vector<double> vec, double p){
 	double sum = 0;
-	for(i = 0; i < sizeof(vec)/sizeof(double); i++){
+	for(int i = 0; i < vec.size(); i++){
 		sum += (p == 1) ? abs(vec[i]) : pow(vec[i], p);
 	}
 	double norm = pow(sum, 1.0/p);
 	return norm;
+}
+
+vvd verif_bottom(vvd a, int m, int n, double dx){
+	for(int i = 0; i < n; i++){
+		a[m-1][i] = (i*dx)*exp(1);
+	}
+
+	return a;
+}
+
+vvd verif_top(vvd a, int n, double dx){
+	for(int i = 0; i < n; i++){
+		a[0][i] = 2.0*exp(i*dx);
+	}
+
+	return a;
+}
+
+vvd verif_left(vvd a, int m, double dy){
+	for(int i = 0; i < m; i++){
+		a[i][0] = 0;
+	}
+
+	return a;
+}
+
+vvd verif_right(vvd a, int m, int n, double dy){
+	for(int i = 0; i < m; i++){
+		a[i][n-1] = 2*exp(i*dy);
+	}
+
+	return a;
 }
